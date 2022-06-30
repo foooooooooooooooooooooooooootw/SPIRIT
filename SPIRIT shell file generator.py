f1642b5182904a244067dd305f671fc72e001a67
foooooooooooooooooooooooooootw <foooooooooooooooooooooooooootw@gmail.com>
2022-06-30 19:27:05 +0800

    add error checking for file name

--- SPIRIT shell file generator.py
+++ SPIRIT shell file generator.py
@@ -4,7 +4,15 @@ import time
 def main():
     current_time = time.strftime("%Y%m%d-%H%M%S")
     filename = input("What's the text file's name?")
-    input_file = open(filename, "r+")
+    filename_error_check = True
+    while filename_error_check:
+        try:
+            input_file = open(filename, "r+")
+            filename_error_check = False
+        except IOError:
+            print("File not found, check directory or file name")
+            filename = input("What's the text file's name?")
+
     lines = input_file.readlines()
     stripped = [line.strip("\n") for line in lines]
 
@@ -16,7 +24,7 @@ def main():
 
     output_file = open("{} - compiled IP ban command.sh".format(current_time), "w")
     output_file.write(compiled)
-    output_file.close
+    output_file.close()
 
 
 main()
